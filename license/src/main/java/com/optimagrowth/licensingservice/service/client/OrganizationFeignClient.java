2866c4b4a9a1912345ee25fc7648afdae1c7ddc6
solvd <solvd@solvds-MacBook-Pro.local>
2022-10-11 17:14:29 +0300

    Eureka is completed

--- license/src/main/java/com/optimagrowth/licensingservice/service/client/OrganizationFeignClient.java
+++ license/src/main/java/com/optimagrowth/licensingservice/service/client/OrganizationFeignClient.java
@@ -2,13 +2,11 @@ package com.optimagrowth.licensingservice.service.client;
 
 import com.optimagrowth.licensingservice.model.Organization;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient("organization-service")
-@Component
 public interface OrganizationFeignClient {
 
     @RequestMapping(
---
+++ organization-service/src/main/java/org/optimagrowth/organizationservice/service/OrganizationService.java
@@ -0,0 +1,40 @@
+package org.optimagrowth.organizationservice.service;
+
+import org.optimagrowth.organizationservice.model.Organization;
+import org.optimagrowth.organizationservice.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @Transactional
+    public Organization findById(String organizationId) {
+        Optional<Organization> opt = organizationRepository.findById(organizationId);
+        return opt.orElse(null);
+    }
+
+    @Transactional
+    public Organization create(Organization organization) {
+        organization.setId(UUID.randomUUID().toString());
+        organization = organizationRepository.save(organization);
+        return organization;
+    }
+
+    @Transactional
+    public void update(Organization organization) {
+        organizationRepository.save(organization);
+    }
+
+    @Transactional
+    public void delete(Organization organization) {
+        organizationRepository.deleteById(organization.getId());
+    }
+}
