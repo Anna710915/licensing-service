2f9e3d8f7ded77ecbab4da9e469561eb4a96e01f
AnnaMerkul <amerkul@solvd.com>
2022-10-18 11:11:03 +0300

    Clean LicenseController

--- license/src/main/java/com/optimagrowth/licensingservice/controller/LicenseController.java
+++ license/src/main/java/com/optimagrowth/licensingservice/controller/LicenseController.java
@@ -1,8 +1,5 @@
 package com.optimagrowth.licensingservice.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import com.optimagrowth.licensingservice.model.License;
 import com.optimagrowth.licensingservice.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +20,6 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId, "");
-//        license.add(linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
-//                linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
-//                linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
-//                linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
